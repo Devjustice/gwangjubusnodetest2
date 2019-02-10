6517b1f5d2d7f61b1e8ada22482280a8e7b37085
Euyin Jeoung <42943520+Devjustice@users.noreply.github.com>
2019-02-10 22:26:20 +0900

    Update busnode.js

--- busnode.js
+++ busnode.js
@@ -1,17 +1,18 @@
 
-var express = require('express');
-var app = express();
 var request = require('request');
 var cheerio = require('cheerio');
-const $url = 'http://api.gwangju.go.kr/xml/arriveInfo';
-const $KEY = '';
-var $station = '2873';
-const $api_url = $url + '?serviceKey=' + $KEY + '&BUSTOP_ID='+$station;
-//console.log($api_url);
+var url = 'http://api.gwangju.go.kr/xml/arriveInfo';
+var queryParams = '?' + encodeURIComponent('ServiceKey') + '=6LlkzoJWKU8inq318FaLpoJU5DnMfCKiuOPaMndL6EhljQAuXZe0%2FUNe0WSAuygwcdufjkYBudRsGHqVmeucyw%3D%3D';  /* Service Key*/
+queryParams += '&' + encodeURIComponent('serviceKey') + '=' + encodeURIComponent(''); /* */
+queryParams += '&' + encodeURIComponent('BUSSTOP_ID') + '=' + encodeURIComponent('2873'); /* 정류소 아이디 */
 
-request($api_url,function(err,res,body){
-
-
+request({
+    url: url + queryParams,
+    method: 'GET'
+}, function (error, response, body) {
+    //console.log('Status', response.statusCode);
+    //console.log('Headers', JSON.stringify(response.headers));
+  console.log('Reponse received', body);
 
 
 
@@ -20,7 +21,14 @@ $('ARRIVE_LIST').each(function(idx){
 let line_name  = $(this).find('LINE_NAME').text();
 let remain_min = $(this).find('REMAIN_MIN').text();
 let arrive_flag = $(this).find('ARRIVE_FLAG').text();
-console.log(`bus: ${line_name} remain: ${remain_min}m  soonarrive: ${arrive_flag}`);
+console.log(`bus: ${line_name} remain: ${remain_min}m  soonarrive: ${arrive_flag}`)
+
+
+
+
+
+
+
 
 });
 });
